ba52b13705b93f6f05452e8912b92a2a39a3500b
Daniel Nascimento <daniel.p.nasciment@gmail.com>
2021-01-19 19:00:25 -0300

    +Up_AnimaScroll

---
+++ js/js_animationscroll.js
@@ -0,0 +1,19 @@
+const elementsToAnimate = document.querySelectorAll('[data-animation]');
+const classOfAnimation = 'complete';
+
+function animationOnScroll() {
+    const currentPositionY = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    elementsToAnimate.forEach(function (element) {
+        if (currentPositionY > element.offsetTop) {
+            element.classList.add('complete');
+        } else {
+// Faz o efeito ser reiniciado
+            element.classList.remove('complete')
+        }
+    })
+}
+
+window.addEventListener('scroll', function () {
+    animationOnScroll();
+})
+
