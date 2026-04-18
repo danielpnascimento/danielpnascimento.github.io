94e18557ace3ec0456604bd70313a8c352e04cc8
danielpnascimento <danielpnascimento.github.io>
2026-04-18 12:11:53 -0300

    Updates Portfólio

--- js/js_preloader.js
+++ js/js_preloader.js
@@ -1,24 +1,9 @@
 //Função para inicialização e desativação do loader para que ele não fique rodando
 //em segundo plano
-//var preloader = document.getElementById('loading');
 var preloader = document.getElementById('loader');
-//function myFunction() {
-//renomado para não conflitar com o mesmo nome da função do leia mais do sobre
 function preFunction() {
     preloader.style.display = 'none';
 
 }
 
-//to make the loader stop after loading page completely 
-//$(document).ready(function () {
-////Preloader
-//    $(window).load(function () {
-//        preloaderFadeOutTime = 500;
-//        function hidePreloader() {
-//            var preloader = $('.spinner-wrapper');
-//            preloader.fadeOut(preloaderFadeOutTime);
-//        }
-//        hidePreloader();
-//    });
-//});
 
